4564198ce82d568b22ce3ba7f632702d8bccfdb6
Dani Alamo <dca5244@psu.edu>
2023-04-09 15:47:28 -0400

    test

--- api/Badge.js
+++ api/Badge.js
@@ -51,6 +51,7 @@ export default async function handler(req, res) {
     
     while(search == ''){
       badges = badges.slice(0,4); 
+      return badges;
     }
     badges.map((schoolBadges) => {
         schoolBadges.index = schoolBadges.fieldName.toLowerCase() + " " + schoolBadges.title.toLowerCase() + " " + schoolBadges.creatorName.toLowerCase();
